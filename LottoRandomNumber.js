6fd14e8026eae18d9852dc6379dc8408040976ea
EunHoLee02 <dldmsgh5280@gmail.com>
2025-04-02 11:10:17 +0900

    로또번호생성

---
+++ LottoRandomNumber.js
@@ -0,0 +1,12 @@
+function getRandomNumbers() {
+    const numbers = [];
+    while (numbers.length < 6) {
+        const randomNum = Math.floor(Math.random() * 45) + 1;
+        if (!numbers.includes(randomNum)) {
+            numbers.push(randomNum);
+        }
+    }
+    return numbers ;
+}
+
+console.log(getRandomNumbers());
